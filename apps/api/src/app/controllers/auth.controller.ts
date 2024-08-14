313b024e58786576d1503c24f7f3a404475ee7e0
Stanislav <stanislavstranger@gmail.com>
2024-08-14 12:30:12 +0300

    feat(api): integrate validation pipes and DTOs for auth endpoints
    - Added `LoginDto` and `RegisterDto` with validation decorators to ensure proper data handling in login and registration requests.
    - Integrated `ValidationPipe` in the `AuthController` for automatic validation of incoming requests.
    - Replaced existing RMQ handling in `AuthController` to utilize `LoginDto` and `RegisterDto` for type-safe and validated communication.
    - Implemented error handling in `AuthController` to return `UnauthorizedException` on failure.

--- apps/api/src/app/controllers/auth.controller.ts
+++ apps/api/src/app/controllers/auth.controller.ts
@@ -1,14 +1,48 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UnauthorizedException,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { AccountLogin } from '@./contracts';
 import { AccountRegister } from '@./contracts';
+import { RMQService } from 'nestjs-rmq';
+import { LoginDto } from '../dtos/login.dto';
+import { RegisterDto } from '../dtos/register.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor() {}
+  constructor(private readonly rmqService: RMQService) {}
 
+  @UsePipes(new ValidationPipe())
   @Post('register')
-  async register(@Body() dto: AccountRegister.Request) {}
+  async register(@Body() dto: RegisterDto) {
+    try {
+      return await this.rmqService.send<
+        AccountRegister.Request,
+        AccountRegister.Response
+      >(AccountRegister.topic, dto);
+    } catch (error) {
+      if (error instanceof Error) {
+        throw new UnauthorizedException(error.message);
+      }
+    }
+  }
 
+  @UsePipes(new ValidationPipe())
   @Post('login')
-  async login(@Body() dto: AccountLogin.Request) {}
+  async login(@Body() dto: LoginDto) {
+    try {
+      return await this.rmqService.send<
+        AccountLogin.Request,
+        AccountLogin.Response
+      >(AccountLogin.topic, dto);
+    } catch (error) {
+      if (error instanceof Error) {
+        throw new UnauthorizedException(error.message);
+      }
+    }
+  }
 }
---
+++ apps/api/src/app/dtos/register.dto.ts
@@ -0,0 +1,8 @@
+import { IsOptional, IsString } from "class-validator";
+import { LoginDto } from "./login.dto";
+
+export class RegisterDto extends LoginDto {
+  @IsOptional()
+  @IsString()
+  displayName?: string;
+}
